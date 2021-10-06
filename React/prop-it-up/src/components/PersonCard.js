496e47684a7a606d6abe58e58139fb3443c9ff5b
Juan Antononio Gonzalez <jgonzalez020801@gmail.com>
2021-10-06 15:11:10 -0700

    commited files putting-it-together

--- React/prop-it-up/src/components/PersonCard.js
+++ React/prop-it-up/src/components/PersonCard.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) =>{
+    const [isAge , setIsAge] = useState(props.age);
+    const birthday = (e) =>{
+        setIsAge(isAge+1);
+    }
     return(
         <div>
             <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {isAge}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={birthday}>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     )
 }
