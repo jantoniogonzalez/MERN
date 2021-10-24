82df325df67e37e50f1e27e67bf61d1aa6d652c9
Juan Antononio Gonzalez <jgonzalez020801@gmail.com>
2021-10-24 11:24:01 -0700

    commited files pokemon-api

--- React/pokemon-api/src/App.js
+++ React/pokemon-api/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
 import PokeBody from './components/PokeBody';
+import axios from 'axios';
+
 
 function App() {
   return (
--- React/pokemon-api/src/components/PokeBody.js
+++ React/pokemon-api/src/components/PokeBody.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const PokeBody = (props) => {
     const [pokemonList, setPokemonList] = useState([])
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=807')
-            .then(response => response.json())
-            .then(response => setPokemonList(response.results))
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+            .then((response)=> 
+                {
+                    console.log(response);
+                    console.log(response.data.results);
+                    setPokemonList(response.data.results);
+                })
+            .catch((err)=>console.log(err))
     }, []);
 
     return (
         <div>
-            {pokemonList.length > 0 && pokemonList.map((pokemon, index)=>{
-                return (<div key={index}>{pokemon.name}</div>)
-            })}
+                {
+                pokemonList.length > 0?
+                pokemonList.map((pokemon, index)=>{
+                    return (<div key={index}>{pokemon.name}</div>)
+                })
+                :null
+            }
         </div>
     );
 }
